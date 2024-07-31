cb898fa30c9dc9cf9173c9e2859c2123b05ce73b
Roland Sebastien <82360425+ROLANDsebastien@users.noreply.github.com>
2024-07-31 09:49:15 +0300

    Finnish assertions section

--- tests/assertion.spec.js
+++ tests/assertion.spec.js
@@ -1,35 +1,62 @@
 import {test, expect} from "@playwright/test";
+import {type} from "node:os";
 
-test.describe("learn assertions", () => {
+test.describe.only("learn assertions", () => {
     test('verify web page behavior', async ({page}) => {
         await page.goto('https://the-internet.herokuapp.com')
         // first assertion to have URL
         await expect(page).toHaveURL('https://the-internet.herokuapp.com')
-        await page.pause()
 
         // to have title
         await expect(page).toHaveTitle('The Internet')
     })
 
-    test('Continue with assertion', async ({page}) => {
+    test('Continue with assertion part 1', async ({page}) => {
         // assertion visibility
         await page.goto('https://the-internet.herokuapp.com')
         await expect(page.locator('h1')).toBeVisible()
-        await page.pause()
 
         // assert element to have text
         await expect(page.locator('h2')).toHaveText('Available Examples')
 
         // assert contains text
         await expect(page.locator('body')).toContainText('WYSIWYG')
-        await page.pause()
     })
 
-    test.only('Continue with assertions part 2', async ({page}) => {
+    test('Continue with assertions part 2', async ({page}) => {
         await page.goto('https://the-internet.herokuapp.com')
 
         // assert count
         await expect(page.locator('a')).toHaveCount(46)
-        await page.pause()
+
+        // to be checked
+        await page.goto('https://the-internet.herokuapp.com/checkboxes')
+
+        await page.waitForLoadState('networkidle')
+
+        await page.getByRole('checkbox').nth(0).check();
+        await page.getByRole('checkbox').nth(1).uncheck();
+
+        await expect(page.getByRole('checkbox').nth(0)).toBeChecked()
+        await expect(page.getByRole('checkbox').nth(1)).not.toBeChecked()
+    })
+
+    test('Continue with assertions part 3', async ({page}) => {
+        await page.goto('https://the-internet.herokuapp.com/login')
+
+        // have value
+        await page.locator('#username').fill('tomsmith')
+        await expect(page.locator('#username')).toHaveValue('tomsmith')
+
+        // element is enable
+        await expect(page.locator('button[type = "submit"]')).toBeEnabled();
+    })
+
+    test('Continue with assertions part 4', async ({page}) => {
+        await page.goto('https://the-internet.herokuapp.com/')
+
+        // verify text store in variable
+        const headerText = await page.locator('h1').textContent()
+        expect(headerText).toBe('Welcome to the-internet')
     })
 })
